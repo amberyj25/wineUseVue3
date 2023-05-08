e54a5d6634af408eeb2d1ef1300e91eb577dbe0b
amberyj25 <amber20240@gmail.com>
2023-05-08 22:11:59 +0800

    carousel component

--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -3,65 +3,65 @@
     <div class="carousel" v-for="(carousel, index) in carouselContent" :key="index">
       <img :src="carousel.img" class="img" alt="img" />
       <div class="alltabs">
-        <i
-          v-for="carouselNum in carouselContentData.length"
-          :key="carouselNum"
-          :class="{fas: true, 'fa-circle': true, fa_circle_style:currentCarouselContentId === carouselNum}"
-          @click="changeTab(carouselNum)"
-        ></i>
+        <i v-for="carouselNum in carouselContentData.length" :key="carouselNum"
+          :class="{ fas: true, 'fa-circle': true, fa_circle_style: currentCarouselId === carouselNum }"
+          @click="changeTab(carouselNum)"></i>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { reactive, ref, onMounted, computed } from '@vue/composition-api'
+
 export default {
   name: 'carousel',
-  data () {
-    return {
-      carouselContentData: [
-        {
-          id: 1,
-          img: 'https://i.imgur.com/6x0J234.jpg'
-        },
-        {
-          id: 2,
-          img: 'https://i.imgur.com/TBHJmjv.jpg'
-        },
-        {
-          id: 3,
-          img: 'https://i.imgur.com/NCbpT18.jpg'
-        }
-      ],
-      currentCarouselContentId: 1
-    }
-  },
-  computed: {
-    carouselContent () {
-      return this.carouselContentData.filter(
-        item => item.id === this.currentCarouselContentId
-      )
-    }
-  },
-  mounted () {
-    this.timer()
-  },
-  methods: {
-    timer () {
-      return setInterval(() => {
-        this.getData()
-      }, 3000)
-    },
-    getData () {
-      if (this.currentCarouselContentId === this.carouselContentData.length) {
-        this.currentCarouselContentId = 1
+  setup() {
+    const currentCarouselId = ref(1);
+    const carouselContentData = reactive([
+      {
+        id: 1,
+        img: 'https://i.imgur.com/6x0J234.jpg'
+      },
+      {
+        id: 2,
+        img: 'https://i.imgur.com/TBHJmjv.jpg'
+      },
+      {
+        id: 3,
+        img: 'https://i.imgur.com/NCbpT18.jpg'
+      }
+    ]);
+
+    onMounted(() => {
+      timer()
+    });
+
+    const carouselContent = computed(() => carouselContentData.filter(
+      item => item.id === currentCarouselId.value
+    ));
+
+    const timer = () => setInterval(() => {
+      getData();
+    }, 3000);
+    const getData = () => {
+      if (currentCarouselId.value === carouselContentData.length) {
+        currentCarouselId.value = 1;
         return
       }
 
-      this.currentCarouselContentId += 1
-    },
-    changeTab (carouselNum) {
-      this.currentCarouselContentId = carouselNum
+      currentCarouselId.value += 1;
+    };
+    const changeTab = (carouselNum) => {
+      currentCarouselId.value = carouselNum;
+    };
+
+    return {
+      carouselContent,
+      carouselContentData,
+      currentCarouselId,
+      timer,
+      changeTab
     }
   }
 }
@@ -69,26 +69,31 @@ export default {
 <style lang="scss" scoped>
 .carousel {
   position: relative;
+
   .img {
     width: 100%;
     height: 590px;
   }
+
   .alltabs {
     width: 100%;
     display: flex;
     justify-content: center;
     position: absolute;
     bottom: 25px;
+
     .fa-circle {
       margin: 0 25px;
       font-size: 0.7rem;
       color: white;
     }
+
     .fa_circle_style {
       color: #0080ff;
     }
   }
 }
+
 @media (min-width: 992px) and (max-width: 1200px) {
   .carousel {
     .img {
@@ -96,6 +101,7 @@ export default {
     }
   }
 }
+
 @media (min-width: 768px) and (max-width: 991px) {
   .carousel {
     .img {
@@ -103,6 +109,7 @@ export default {
     }
   }
 }
+
 @media (min-width: 576px) and (max-width: 767px) {
   .carousel {
     .img {
@@ -110,6 +117,7 @@ export default {
     }
   }
 }
+
 @media (max-width: 575px) {
   .carousel {
     .img {
