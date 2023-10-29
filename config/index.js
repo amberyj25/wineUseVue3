bcbb5c978f8e8dd9f6164e276574a3aff1360ba0
amberyj25 <amber20240@gmail.com>
2023-10-29 19:07:29 +0800

    後台管理初版 ＆ ECharts

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/wineSpace/dist/',
+    assetsPublicPath: '/wineUseVue3/dist/',
 
     /**
      * Source Maps
---
+++ src/views/backend/MemberInfo.vue
@@ -0,0 +1,54 @@
+<template>
+  <div style="display: flex;">
+    <div>
+      <Sidebar />
+    </div>
+    <div style="width: 1000px;">
+      <div class="title">管理員</div>
+      <b-table striped hover :items="memberInfo"></b-table>
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed } from '@vue/composition-api'
+import { useStore } from "@/store"
+import Sidebar from '@/components/Sidebar.vue'
+
+export default {
+  name: 'MemberInfo',
+  components: {
+    Sidebar
+  },
+  setup() {
+    const store = useStore();
+    const memberInfo = computed(() => {
+      return [
+        {
+          id: 1,
+          name: 'Amber',
+          email: 'amber@gmail.com'
+        }, {
+          id: 2,
+          name: 'Leopoldo',
+          email: 'leopoldo@gmail.com'
+        }, {
+          id: 3,
+          name: 'Delphine',
+          email: 'delphine@gmail.com'
+        }
+      ]
+    });
+
+    return {
+      memberInfo,
+    }
+  }
+}
+</script>
+
+<style  lang="scss" scoped>
+.title {
+  font-size: 35px;
+}
+</style>
